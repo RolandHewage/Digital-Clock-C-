a51813d245574d0f90edc817d0bc419b5342763a
RolandHewage <44081958+RolandHewage@users.noreply.github.com>
2018-10-12 16:14:28 +0530

    Folder 1
    Folder 1

---
+++ DigitalClock/Prac1101/Prac1101/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Prac1101
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.txtSec = new System.Windows.Forms.TextBox();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.txtHrs = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.Color.Black;
+            this.panel.Controls.Add(this.label4);
+            this.panel.Controls.Add(this.label3);
+            this.panel.Controls.Add(this.label2);
+            this.panel.Controls.Add(this.label1);
+            this.panel.Location = new System.Drawing.Point(25, 27);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(500, 500);
+            this.panel.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(3, 241);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "9";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(237, 474);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "6";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(482, 241);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(237, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "12";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(592, 226);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(592, 278);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // txtSec
+            // 
+            this.txtSec.Location = new System.Drawing.Point(604, 27);
+            this.txtSec.Name = "txtSec";
+            this.txtSec.Size = new System.Drawing.Size(100, 20);
+            this.txtSec.TabIndex = 4;
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(604, 67);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(100, 20);
+            this.txtMin.TabIndex = 5;
+            // 
+            // txtHrs
+            // 
+            this.txtHrs.Location = new System.Drawing.Point(604, 108);
+            this.txtHrs.Name = "txtHrs";
+            this.txtHrs.Size = new System.Drawing.Size(100, 20);
+            this.txtHrs.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(544, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Sec";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(544, 70);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Min";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(544, 108);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Hrs";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 575);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtHrs);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.txtSec);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.panel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox txtSec;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.TextBox txtHrs;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ DigitalClock/Prac1101/Prac1101/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prac1101
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double[,] x = new double[3, 4];
+        double[,] y = new double[3, 4];
+        int[] points = new int[3];
+
+        private void DisplayPoint(float x, float y)
+        {
+            Graphics g = panel.CreateGraphics();
+            g.DrawLine(Pens.White, x, panel.Height - y, x + 0.1f, panel.Height - y);
+        }
+
+        private void DeletePoint(float x, float y)
+        {
+            Graphics g = panel.CreateGraphics();
+            g.DrawLine(Pens.Black, x, panel.Height - y, x + 0.1f, panel.Height - y);
+        }
+
+        private void DisplayLine(float x1, float y1, float x2, float y2)
+        {
+            Graphics g = panel.CreateGraphics();
+            if ((x1 == x2) && (y1 == y2))
+            {
+                DisplayPoint(x1, y1);
+            }
+            else
+            {
+                g.DrawLine(Pens.White, x1, panel.Height - y1, x2, panel.Height - y2);
+            }
+        }
+
+        private void DeleteLine(float x1, float y1, float x2, float y2)
+        {
+            Graphics g = panel.CreateGraphics();
+            if ((x1 == x2) && (y1 == y2))
+            {
+                DeletePoint(x1, y1);
+            }
+            else
+            {
+                g.DrawLine(Pens.Black, x1, panel.Height - y1, x2, panel.Height - y2);
+            }
+        }
+
+        private void DrawPolygon(int o)
+        {
+            int i, j;
+            for (i = 0; i < points[o]; i++)
+            {
+                j = (i + 1) % points[o];
+                DisplayLine((float)x[o, i], (float)y[o, i], (float)x[o, j], (float)y[o, j]);
+            }
+        }
+
+        private void DeletePolygon(int o)
+        {
+            int i, j;
+            for (i = 0; i < points[o]; i++)
+            {
+                j = (i + 1) % points[o];
+                DeleteLine((float)x[o, i], (float)y[o, i], (float)x[o, j], (float)y[o, j]);
+            }
+        }
+
+        void translate(int o, int i, double tx, double ty)
+        {
+            x[o, i] += tx;
+            y[o, i] += ty;
+        }
+
+        void rotate(int o, int i, double t)
+        {
+            double x1, y1;
+
+            x1 = x[o, i];
+            y1 = y[o, i];
+            x[o, i] = x1 * Math.Cos(t) - y1 * Math.Sin(t);
+            y[o, i] = x1 * Math.Sin(t) + y1 * Math.Cos(t);
+        }
+
+        void f_rotate(int o, int i, double t, double x, double y)
+        {
+            translate(o, i, -x, -y);
+            rotate(o, i, t);
+            translate(o, i, x, y);
+        }
+
+        void scale(int o, int i, double sx, double sy)
+        {
+            x[o, i] *= sx;
+            y[o, i] *= sy;
+        }
+
+        void f_scale(int o, int i, double sx, double sy, double x, double y)
+        {
+            translate(o, i, -x, -y);
+            scale(o, i, sx, sy);
+            translate(o, i, x, y);
+        }
+       
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            /*int i, n;
+
+            x[0, 0] = -5; y[0, 0] = 0;
+            x[0, 1] = 0; y[0, 1] = -5;
+            x[0, 2] = 5; y[0, 2] = 0;
+            x[0, 3] = 0; y[0, 3] = 200;
+
+            points[0] = 4;
+
+            x[1, 0] = -5; y[1, 0] = 0;
+            x[1, 1] = 0; y[1, 1] = -5;
+            x[1, 2] = 5; y[1, 2] = 0;
+            x[1, 3] = 0; y[1, 3] = 150;
+
+            points[1] = 4;
+
+            x[2, 0] = -5; y[2, 0] = 0;
+            x[2, 1] = 0; y[2, 1] = -5;
+            x[2, 2] = 5; y[2, 2] = 0;
+            x[2, 3] = 0; y[2, 3] = 100;
+
+            points[2] = 4;
+
+            for (i = 0; i < 4; i++)
+            {
+                translate(0, i, 250, 250);
+                translate(1, i, 250, 250);
+                translate(2, i, 250, 250);
+            }
+            DrawPolygon(0);
+            DrawPolygon(1);
+            DrawPolygon(2);
+            for (n = 1; ; n++)
+            {
+
+                
+                System.Threading.Thread.Sleep(1000);
+                DeletePolygon(0);
+
+                for (i = 0; i < 4; i++)
+                {
+                    f_rotate(0, i, -0.10472, 250, 250);
+                }
+                DrawPolygon(0);
+
+                if ((n % 60) == 0)
+                {
+                    DeletePolygon(1);
+                    for (i = 0; i < 4; i++)
+                    {
+                        f_rotate(1, i, -0.10472, 250, 250);
+                    }
+                    DrawPolygon(1);
+                }
+
+                if ((n % 3600) == 0)
+                {
+                    DeletePolygon(2);
+                    for (i = 0; i < 4; i++)
+                    {
+                        f_rotate(2, i, -0.10472, 250, 250);
+                    }
+                    DrawPolygon(2);
+                }
+
+            }*/
+
+            _t = new System.Threading.Thread(new System.Threading.ThreadStart(DoSomething));
+            _t.Start();
+        }
+
+        private System.Threading.Thread _t;
+
+        private void DoSomething()
+        {
+            int i, n;
+
+            x[0, 0] = -5; y[0, 0] = 0;
+            x[0, 1] = 0; y[0, 1] = -5;
+            x[0, 2] = 5; y[0, 2] = 0;
+            x[0, 3] = 0; y[0, 3] = 200;
+
+            points[0] = 4;
+
+            x[1, 0] = -5; y[1, 0] = 0;
+            x[1, 1] = 0; y[1, 1] = -5;
+            x[1, 2] = 5; y[1, 2] = 0;
+            x[1, 3] = 0; y[1, 3] = 150;
+
+            points[1] = 4;
+
+            x[2, 0] = -5; y[2, 0] = 0;
+            x[2, 1] = 0; y[2, 1] = -5;
+            x[2, 2] = 5; y[2, 2] = 0;
+            x[2, 3] = 0; y[2, 3] = 100;
+
+            points[2] = 4;
+
+            int sec = int.Parse(txtSec.Text);
+            int min = int.Parse(txtMin.Text);
+            int hrs = int.Parse(txtHrs.Text);
+            for (i = 0; i < 4; i++)
+            {
+                rotate(0, i, -0.10472*sec);
+                rotate(1, i, -0.10472*min);
+                rotate(2, i, -0.10472*hrs*5);
+            }
+
+            for (i = 0; i < 4; i++)
+            {
+                translate(0, i, 250, 250);
+                translate(1, i, 250, 250);
+                translate(2, i, 250, 250);
+            }
+            DrawPolygon(0);
+            DrawPolygon(1);
+            DrawPolygon(2);
+            for (n = 1*sec; ; n++)
+            {
+                
+                System.Threading.Thread.Sleep(1000);
+
+                DeletePolygon(0);
+                for (i = 0; i < 4; i++)
+                {
+                    f_rotate(0, i, -0.10472, 250, 250);
+                }
+                /*
+                DeletePolygon(1);
+                for (i = 0; i < 4; i++)
+                {
+                    f_rotate(1, i, -0.10472/60, 250, 250);
+                }
+                DeletePolygon(2);
+                for (i = 0; i < 4; i++)
+                {
+                    f_rotate(2, i, -0.10472/3600, 250, 250);
+                }
+                DrawPolygon(0);
+                DrawPolygon(1);
+                DrawPolygon(2);
+                */
+                DrawPolygon(0);
+
+                if ((n % 60) == 0)
+                {
+                    DeletePolygon(1);
+                    for (i = 0; i < 4; i++)
+                    {
+                        f_rotate(1, i, -0.10472, 250, 250);
+                    }
+                    DrawPolygon(1);
+                }
+                else
+                {
+                    DrawPolygon(1);
+                }
+
+                if (((n+(min*60)) % 3600) == 0)
+                {
+                    DeletePolygon(2);
+                    for (i = 0; i < 4; i++)
+                    {
+                        f_rotate(2, i, -0.10472*5, 250, 250);
+                    }
+                    DrawPolygon(2);
+                }
+                else
+                {
+                    DrawPolygon(2);
+                }
+
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            _t.Abort();
+            panel.Invalidate();
+        }
+    }
+}
